f21881a2ef03c5c053b88558a376aa199fd93970
Casa <Casa@192.168.1.128>
2014-06-02 12:31:11 +0200

    nueva clase comun dragos

---
+++ src/paqueteDragosLupu/ModificadaPorAmbos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteDragosLupu;
+
+/**
+ *
+ * @author Casa
+ */
+public class ModificadaPorAmbos {
+    
+    public void metodoDragos()
+    {
+        
+    }
+    
+}
---
+++ src/paqueteNachoMovellan/ClaseNachoMovellan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteNachoMovellan;
+
+/**
+ *
+ * @author Casa
+ */
+public class ClaseNachoMovellan {
+    static public void main(String[] args)
+    {
+        System.out.println("hola mundo");
+        System.out.println("nueva modificacion");
+        System.out.println("nueva mo");
+        System.out.println("nununu");
+    }        
+    
+}
